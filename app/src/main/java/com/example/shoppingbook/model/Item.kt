fa424a9600bc709f80bc692e8e63e8bc4e736125
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-29 17:35:21 +0300

    ItemModel

---
+++ app/src/main/java/com/example/shoppingbook/model/Item.kt
@@ -0,0 +1,21 @@
+package com.example.shoppingbook.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Item(
+    @ColumnInfo("name")
+    var itemName : String,
+    @ColumnInfo("storename")
+    var storeName : String?,
+    @ColumnInfo("price")
+    var price : String?,
+    @ColumnInfo("image")
+    var image : ByteArray?
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    var id : Int = 0
+}
