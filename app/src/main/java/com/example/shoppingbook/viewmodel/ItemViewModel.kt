61ce8f050c01c9a8116818899ccb466030116f43
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-30 20:03:38 +0300

    itemviewmodel funs

--- app/src/main/java/com/example/shoppingbook/viewmodel/ItemViewModel.kt
+++ app/src/main/java/com/example/shoppingbook/viewmodel/ItemViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.shoppingbook.viewmodel
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.room.Room
+import com.example.shoppingbook.model.Item
 import com.example.shoppingbook.roomdb.ItemDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ItemViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -15,7 +20,36 @@ class ItemViewModel(application: Application) : AndroidViewModel(application) {
 
     private val itemDao = db.userDao()
 
-    fun getItems(){
-        itemDao.getItemWithNameAndId()
+    val itemList = mutableStateOf<List<Item>>(listOf())
+    val selectedItem = mutableStateOf<Item>(Item("","","", ByteArray(1)))
+
+
+
+    fun getItemList(){
+        viewModelScope.launch(Dispatchers.IO) {
+            itemList.value = itemDao.getItemWithNameAndId()
+
+        }
+    }
+
+    fun getItem(id : Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            val item = itemDao.getItemById(id)
+            item?.let {
+                selectedItem.value = it
+            }
+        }
+    }
+
+    fun saveItem(item : Item){
+        viewModelScope.launch(Dispatchers.IO) {
+            itemDao.insert(item)
+        }
+    }
+
+    fun deleteItem(item: Item){
+        viewModelScope.launch(Dispatchers.IO) {
+            itemDao.delete(item)
+        }
     }
 }
