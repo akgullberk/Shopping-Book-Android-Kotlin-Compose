5a96287fb82d546ec62104447c017d5a3aa47893
Berk Akgül <berkak_gul51@hotmail.com>
2024-11-01 17:13:49 +0300

    ImagePicker

--- app/src/main/java/com/example/shoppingbook/screens/AddItemScreen.kt
+++ app/src/main/java/com/example/shoppingbook/screens/AddItemScreen.kt
@@ -1,5 +1,11 @@
 package com.example.shoppingbook.screens
 
+import android.Manifest
+import android.net.Uri
+import android.os.Build
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,11 +16,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import com.example.shoppingbook.model.Item
 
 @Composable
@@ -97,5 +106,36 @@ fun AddItemScreen(saveFunction : () -> Unit){
                 Text(text = "Save")
             }
         }
+    }
+}
+
+@Composable
+fun ImagePicker(){
+    var selectedImageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+
+    val permission = if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.TIRAMISU){
+        Manifest.permission.READ_MEDIA_IMAGES
+    }else{
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    val galleryLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
+
+        selectedImageUri = uri
+        
+    }
+
+    val permissionLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) { granted ->
+        if(granted){
+            galleryLauncher.launch("image/*")
+        }else{
+            Toast.makeText(context,"Permission Denied",Toast.LENGTH_LONG).show()
+        }
+
+
+        
     }
 }
